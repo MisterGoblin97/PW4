540fe1784984ad33430f47376cc59ab52d9cd4a9
MisterGoblin96 <krestinenko.matvey@gmail.com>
2023-03-24 21:43:05 +0300

    Добавьте файлы проекта.

---
+++ PW4.1/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace PW4._1
+{
+    class Spell 
+    {
+        public string Name { get; set; }
+        public int Mana { get; private set; }
+        private string Effect { get; set; }
+
+        public Spell (string name, int mana, string effect)
+        {
+            Name = name;
+            Mana = mana;
+            Effect = effect;
+        }
+
+        public string use() 
+        {
+           return Effect;    
+        }
+    }
+
+    class Magician
+    { 
+        public string Name { get; private set; }
+        public int Mana { get; private set; }
+
+        public Magician(string name, int mana) 
+        {
+            Name = name;
+            Mana = mana;
+        }
+
+        public void CastSpell(Spell spell)
+        {
+            if (Mana <= spell.Mana)
+            {
+                int manaNeed = spell.Mana - Mana;
+                Console.WriteLine($"Для использования {spell.Name} не хватает {manaNeed} едениц маны. Хлебни зелья! хехе");
+                
+            }
+            else
+            { 
+                spell.use();
+                Mana -= spell.Mana;
+
+                Console.WriteLine($"Колдун {Name} колдует! {spell.use()}");
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main()
+        {
+            Spell FireBall = new Spell("Огненный шар", 49, "Огненый шар взорвался, это четко.");
+            Spell ParseTwoString = new Spell("запарсить пару строчек", 50, "Строки запарились?? Ну нормульче");
+
+            Magician Krestinenko = new Magician("Крестиненко", 50);
+            Magician Mertsalov = new Magician("Мерцалов Сергей Александрович", 49);
+
+            Krestinenko.CastSpell(FireBall);
+            Mertsalov.CastSpell(ParseTwoString);
+        }
+    }
+}
---
+++ PW4.2/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace PW4._2
+{
+    class Package
+    { 
+        public string PackageDiscription { get; private set; }
+        public double Weight { get; private set; }
+
+        public Package (string packageDiscription, double weight)
+        {
+            PackageDiscription = packageDiscription;
+            Weight = weight;
+        }
+    }
+
+    class DeliveryService
+    {
+        public double WeightLimit { get; private set; }
+
+        public DeliveryService(double weightLimit)
+        {
+            WeightLimit = weightLimit;
+        }
+
+        public void SendPackage(Package package)
+        {
+            if (package.Weight >= WeightLimit)
+            {
+                Console.WriteLine($"Вес посылки слишком велик, он равен: {package.Weight}, а лимит равен: {WeightLimit}");
+            }
+            else
+            {
+                Console.WriteLine($"Ваша посылка {package.PackageDiscription} с весом {package.Weight} успешно отправлена!");
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main()
+        {
+            DeliveryService KrestinenkoDelivery = new DeliveryService(150);
+
+            Package firstPack = new Package("Новый карбюратор на восьмерку", 20);
+            Package secondPack = new Package("Здоровая бутылка пива", 250);
+
+            KrestinenkoDelivery.SendPackage(firstPack);
+            KrestinenkoDelivery.SendPackage(secondPack);
+        }
+    }
+}
